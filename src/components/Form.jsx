d9a9b41a027f89c7e9f63467e380939379376ddc
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2022-05-19 15:59:29 +0530

    demo axios code may need modifications

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,30 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 function Form() {
     const[sepalLength, setSepalLength] = useState("");
     const[sepalWidth, setSepalWidth] = useState("");
     const[petalLength, setPetalLength] = useState("");
     const[petalWidth, setPetalWidth] = useState("");
+    const[result, setResult] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert(sepalLength + " " + sepalWidth + " " + petalLength + " "  + petalWidth);
+        // alert(sepalLength + " " + sepalWidth + " " + petalLength + " "  + petalWidth);
+        axios({
+            method: 'GET',
+            url: 'https://jsonplaceholder.typicode.com/todos/1',
+            data: {
+                sepalLength: sepalLength,
+                sepalWidth: sepalWidth,
+                petalLength: petalLength,
+                petalWidth: petalWidth
+            }
+        }).then(res => setResult(res.data))
     }
 
     return (
+        <>
         <div className="form-submit">
             <form onSubmit={handleSubmit}>
                 <label>Sepal Length
@@ -48,6 +61,10 @@ function Form() {
                 <input type="submit"/>
             </form>
         </div>
+        <div className="result">
+            Prediction is = {result}
+        </div>
+        </>
     )
 }
 
